3381db67775306736550ea3f8aa3f09581263f75
Branislav Hesko <branislav.hesko@protonmail.ch>
2022-07-06 20:22:29 +0200

    minor improvement

--- segmentation_voxel/config/config.py
+++ segmentation_voxel/config/config.py
@@ -1,4 +1,7 @@
 import dataclasses
+from typing import Tuple
+
+from torch import dropout
 
 from segmentation_voxel.config.mode import DataMode
 
@@ -13,8 +16,20 @@ class TrainingConfig:
     }
     learning_rate: float =  0.001
     num_epochs: int = 20
+    dropout_probability: float = 0.1
+    
+@dataclasses.dataclass()
+class ModelConfig:
+    model: str = "small"
+    embed_size: int = 96
+    num_heads: int = 8
+    input_channels: int = 3
+    patch_size: int = 16
+    input_shape: Tuple[int, int, int] = (3, 128, 128, 128)
 
 @dataclasses.dataclass()
 class Config:
     path: str = "/home/brani/DATASETS/Zebrafish (NucMM-Z)"
     training_config: TrainingConfig = TrainingConfig()
+    model_config: ModelConfig = ModelConfig()
+    num_classes: int = 2
--- segmentation_voxel/training/segmentation_trainer.py
+++ segmentation_voxel/training/segmentation_trainer.py
@@ -5,6 +5,7 @@ from tqdm import tqdm
 from segmentation_voxel.config.config import Config
 from segmentation_voxel.config.mode import DataMode
 from segmentation_voxel.data.dataset_nucmm import get_data_loaders
+from segmentation_voxel.modeling.voxel_former import SegmentationTransformer3D
 
 
 class SegmentationTrainer:
@@ -17,6 +18,16 @@ class SegmentationTrainer:
             lr=config.training_config.learning_rate, 
             weight_decay=0.0001, 
             amsgrad=True)
+        num_patches = self.config.model_config.input[1] * self.config.model_config.input_shape[2] * self.config.model_config.input_shape[3] // (self.config.model_config.patch_size ** 3)
+        self.model = SegmentationTransformer3D(
+            num_classes=self.config.num_classes,
+            embed_size=self.config.model_config.embed_size,
+            num_heads=self.config.model_config.num_heads,
+            input_channels=self.config.model_config.input_channels,
+            patch_size=self.config.model_config.patch_size,
+            input_shape=num_patches,
+            dropout=self.config.training_config.dropout_probability
+        )
     
     def train(self):
         
